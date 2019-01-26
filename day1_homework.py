32c4ab713fb1a8a6b41974428b430d8359bca50c
KooSung <34935911+KooSung@users.noreply.github.com>
2019-01-26 21:49:19 +0800

    Datawhale

--- day1_homework.py
+++ day1_homework.py
@@ -4,4 +4,6 @@
 name = input('请输入姓名：')
 gender = input('请输入性别：')
 age = input('请输入年龄：')
-print("您的姓名是：%s ，您的性别是：%s ，您的年龄是：%s。"%(name, gender, age))
+
+year = 2019 - int(age) - 1
+print("您的姓名是：%s ，您的性别是：%s ，您是%s出生的。"%(name, gender, year))
